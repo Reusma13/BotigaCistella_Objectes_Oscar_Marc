1f86250a93068b2fc7a25e01af289ead20e5cfdb
Marc Vancea <marc.victor.vancea@gmail.com>
2024-03-06 12:23:32 +0100

    Documentació i Comentaris de la Cistella

---
+++ BotigaCistella_MarcVancea_OscarReus/BotigaCistella_MarcVancea_OscarReus/Cistella.cs
@@ -0,0 +1,301 @@
+﻿using System;
+namespace BotigaCistella_MarcVancea_OscarReus
+{
+	public class Cistella
+	{
+        // Atributs
+        private Botiga botiga; // La botiga on es realitza la compra
+        private DateTime data; // La data de la compra
+        private Producte[] productes; // Els productes que es compren
+        private int[] quantitat; // La quantitat de cada producte que es compra
+        private int nElements; // El nombre total d'elements a la cistella
+        private decimal diners; // Els diners disponibles per a la compra
+
+        // Propietats
+
+        /// <summary>
+        /// Mètode per afegir un producte a la cistella
+        /// </summary>
+        /// <param name="producte">array d'elements (llistat dels productes que es compren)</param>
+        /// <param name="quantitat">int (indica la quantitat de productes que tindrem)</param>
+        /// <returns>retorna un boolea depenguent de si hi ha espai a la cistella o o</returns>
+        public bool AfegirProducte(Producte producte, int quantitat)
+        {
+            // Comprova si hi ha espai a la cistella i si hi ha suficients diners
+            if (nElements < productes.Length && producte.Preu * quantitat <= diners)
+            {
+                // Afegeix el producte a la cistella
+                this.productes[nElements] = producte;
+                this.quantitat[nElements] = quantitat;
+                this.nElements++;
+                this.diners -= producte.Preu * quantitat;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Mètode per obtenir el nombre total d'elements a la cistella
+        /// </summary>
+        /// <returns>retorna el numero del elements</returns>
+        public int ObtenirNElements()
+        {
+            return this.nElements;
+        }
+
+        /// <summary>
+        /// Mètode per obtenir els diners restants
+        /// </summary>
+        /// <returns>retorna els diners que tens</returns>
+        public decimal ObtenirDiners()
+        {
+            return this.diners;
+        }
+
+        /// <summary>
+        /// Mètode per obtenir la data de la compra
+        /// </summary>
+        /// <returns>retorna la data</returns>
+        public DateTime ObtenirData()
+        {
+            return this.data;
+        }
+
+        // Constructor buit
+        public Cistella()
+        {
+            this.botiga = null;
+            this.data = DateTime.Now;
+            this.productes = new Producte[10];
+            this.quantitat = new int[10];
+            this.nElements = 0;
+            this.diners = 0m;
+        }
+
+        // Constructor amb botiga, productes, quantitats i diners
+        public Cistella(Botiga botiga, Producte[] productes, int[] quantitat, decimal diners)
+        {
+            // Comprova si les taules de productes i quantitats tenen la mateixa mida
+            if (productes.Length == quantitat.Length)
+            {
+                // Calcula el total de la compra
+                decimal total = 0m;
+                for (int i = 0; i < productes.Length; i++)
+                {
+                    total += productes[i].ObtenirPreu() * quantitat[i];
+                }
+
+                // Comprova si hi ha suficients diners per a la compra
+                if (total <= diners)
+                {
+                    this.botiga = botiga;
+                    this.data = DateTime.Now;
+                    this.productes = productes;
+                    this.quantitat = quantitat;
+                    this.nElements = productes.Length;
+                    this.diners = diners - total;
+                }
+            }
+        }
+
+        // Mètodes
+
+
+        /// <summary>
+        /// Mètode per comprar un producte mirant si hi ha espai a la cistella i si tenim suficients diners per fer la compra
+        /// </summary>
+        /// <param name="producte">array d'elements (llistat dels productes que es compren)</param>
+        /// <param name="quantitat">int (indica la quantitat de productes que tindrem)</param>
+        /// <returns>retorna un boolea si ha sigut posible fer la compra del producte</returns>
+        public bool ComprarProducte(Producte producte, int quantitat)
+        {
+            // Comprova si el producte existeix a la botiga
+            if (!botiga.ExisteixProducte(producte))
+            {
+                Console.WriteLine("El producte no existeix a la botiga.");
+                return false;
+            }
+
+            // Comprova si hi ha espai a la cistella
+            if (nElements >= productes.Length)
+            {
+                Console.WriteLine("No hi ha espai a la cistella. Voleu ampliar la cistella?");
+                return false;
+            }
+
+            // Comprova si hi ha suficients diners per comprar el producte
+            if (producte.Preu * quantitat > diners)
+            {
+                Console.WriteLine("No teniu suficients diners per comprar aquest producte. Voleu ingressar més diners?");
+                return false;
+            }
+
+            // Afegeix el producte a la cistella
+            this.productes[nElements] = producte;
+            this.quantitat[nElements] = quantitat;
+            this.nElements++;
+            this.diners -= producte.Preu * quantitat;
+
+            // Modifica la data
+            this.data = DateTime.Now;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Mètode per afegir un conjunt de productes amb els seus respectives quantitats.
+        /// </summary>
+        /// <param name="productes">array d'elements (llistat dels productes que es compren)</param>
+        /// <param name="quantitats">int (indica la quantitat de productes que tindrem)</param>
+        /// <returns>retorna un boolea per comprovar si s'ha pogut fer la compra</returns>
+        public bool ComprarProducte(Producte[] productes, int[] quantitats)
+        {
+            // Comprova si les taules de productes i quantitats tenen la mateixa mida
+            if (productes.Length != quantitats.Length)
+            {
+                Console.WriteLine("Les taules de productes i quantitats han de tenir la mateixa mida.");
+                return false;
+            }
+
+            // Per cada producte a la llista de productes
+            for (int i = 0; i < productes.Length; i++)
+            {
+                // Comprova si el producte existeix a la botiga
+                if (!botiga.ExisteixProducte(productes[i]))
+                {
+                    Console.WriteLine("El producte " + productes[i].ObtenirNom() + " no existeix a la botiga.");
+                    return false;
+                }
+
+                // Comprova si hi ha espai a la cistella
+                if (nElements >= this.productes.Length)
+                {
+                    Console.WriteLine("No hi ha espai a la cistella. Voleu ampliar la cistella?");
+                    return false;
+                }
+
+                // Comprova si hi ha suficients diners per comprar el producte
+                if (productes[i].Preu * quantitats[i] > diners)
+                {
+                    Console.WriteLine("No teniu suficients diners per comprar el producte " + productes[i].ObtenirNom() + ". Voleu ingressar més diners?");
+                    return false;
+                }
+
+                // Afegeix el producte a la cistella
+                this.productes[nElements] = productes[i];
+                this.quantitat[nElements] = quantitats[i];
+                this.nElements++;
+                this.diners -= productes[i].Preu * quantitats[i];
+
+                // Modificar la data
+                this.data = DateTime.Now;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Mètode que ordena la nostra cistella per ordre alfabètic amb elmètode de bombolla
+        /// </summary>
+        public void OrdernarCistella()
+        {
+            // Implementació del mètode de la bombolla
+            for (int i = 0; i < nElements - 1; i++)
+            {
+                for (int j = 0; j < nElements - i - 1; j++)
+                {
+                    // Comprova si el producte actual és major que el següent
+                    if (productes[j].ObtenirNom().CompareTo(productes[j + 1].ObtenirNom()) > 0)
+                    {
+                        // Intercanvia els productes
+                        Producte tempProducte = productes[j];
+                        productes[j] = productes[j + 1];
+                        productes[j + 1] = tempProducte;
+
+                        // Intercanvia les quantitats
+                        int tempQuantitat = quantitat[j];
+                        quantitat[j] = quantitat[j + 1];
+                        quantitat[j + 1] = tempQuantitat;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mètode que fa visible la nostra cistella i la fa bonica per l'usuari
+        /// </summary>
+        public void Mostra()
+        {
+            Console.WriteLine("TIQUET DE COMPRA");
+            Console.WriteLine("Botiga: " + botiga.ObtenirNom());
+            Console.WriteLine("Data: " + data.ToString());
+
+            decimal total = 0m;
+            for (int i = 0; i < nElements; i++)
+            {
+                decimal preuTotal = productes[i].ObtenirPreu() * quantitat[i];
+                total += preuTotal;
+                Console.WriteLine("Producte: " + productes[i].ObtenirNom());
+                Console.WriteLine("Quantitat: " + quantitat[i]);
+                Console.WriteLine("Preu Unitari: " + productes[i].ObtenirPreu());
+                Console.WriteLine("Preu Total: " + preuTotal);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Diners Restants: " + diners);
+        }
+
+        /// <summary>
+        /// Mètode per calcular el cost total
+        /// </summary>
+        /// <returns>retorna el total del cost total de la cistella</returns>
+        public decimal CostTotal()
+        {
+            decimal total = 0m;
+            for (int i = 0; i < nElements; i++)
+            {
+                total += productes[i].ObtenirPreu() * quantitat[i];
+            }
+
+            total *= 1.21m;
+
+            return total;
+        }
+
+        /// <summary>
+        /// Mètode que passa tot a un string
+        /// </summary>
+        /// <returns>retorna el tiquet en format string</returns>
+        public override string ToString()
+        {
+            string tiquet = "";
+
+            tiquet += "TIQUET DE COMPRA\n";
+            tiquet += "Botiga: " + botiga.ObtenirNom() + "\n";
+            tiquet += "Data: " + data.ToString() + "\n";
+
+            decimal total = 0m;
+            for (int i = 0; i < nElements; i++)
+            {
+                decimal preuTotal = productes[i].ObtenirPreu() * quantitat[i];
+                total += preuTotal;
+                tiquet += "Producte: " + productes[i].ObtenirNom() + "\n";
+                tiquet += "Quantitat: " + quantitat[i] + "\n";
+                tiquet += "Preu Unitari: " + productes[i].ObtenirPreu() + "\n";
+                tiquet += "Preu Total: " + preuTotal + "\n\n";
+            }
+
+            total *= 1.21m;
+
+            tiquet += "Total amb IVA inclòs: " + total + "\n";
+
+            return tiquet;
+        }
+
+    }
+}
+
