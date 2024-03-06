80f43e07ad6414be6b6bc2625ffc9a850ec6b043
Reusma13 <a2308764@institutmontilivi.cat>
2024-03-06 10:58:26 +0100

    Afegir doucment per començar

---
+++ BotigaCistella_MarcVancea_OscarReus/BotigaCistella_MarcVancea_OscarReus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BotigaCistella_MarcVancea_OscarReus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
